91fdf7bc91439c55613b3be47cbfe246045ddbba
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-11-02 20:46:53 -0600

    add words and remove it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+import { fromEvent } from 'rxjs';
+
+const letterRow = document.getElementsByClassName('letter-row');
+const onKeyDown$ = fromEvent(document, 'keydown');
+let letterRowIndex = 0;
+let letterIndex = 0;
+
+const insertLetter = {
+  next: (event) => {
+    if (event.key === 'Backspace') {
+      letterIndex--;
+      if (letterIndex < 0) letterIndex = 0;
+      if (letterIndex >= 0) {
+        const box = Array.from(letterRow)[letterRowIndex].children[letterIndex];
+        box.textContent = '';
+        box.classList.remove('filled-letter');
+      }
+    }
+    if (event.code.includes('Key')) {
+      if (letterIndex <= 4) {
+        const box = Array.from(letterRow)[letterRowIndex].children[letterIndex];
+        box.textContent = event.key;
+        box.classList.add('filled-letter');
+        letterIndex++;
+      }
+    }
+  },
+};
+
+onKeyDown$.subscribe(insertLetter);
