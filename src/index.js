89d8df374753c59fece01219d1f472c7cf5574eb
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-11-03 22:17:58 -0600

    subject to make the words green and generate a random word

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import { fromEvent } from 'rxjs';
+import { fromEvent, Subject } from 'rxjs';
+import WORDS_LIST from './wordsList.json';
 
 const letterRow = document.getElementsByClassName('letter-row');
 const onKeyDown$ = fromEvent(document, 'keydown');
+const onUserWinOrLose$ = new Subject();
 let letterRowIndex = 0;
 let letterIndex = 0;
+let userAnswer = [];
+const getRandomWord = () => {
+  return WORDS_LIST[Math.floor(Math.random() * WORDS_LIST.length)];
+};
+const rightWord = getRandomWord();
 
 const insertLetter = {
   next: (event) => {
@@ -14,6 +21,7 @@ const insertLetter = {
         const box = Array.from(letterRow)[letterRowIndex].children[letterIndex];
         box.textContent = '';
         box.classList.remove('filled-letter');
+        userAnswer.pop();
       }
     }
     if (event.code.includes('Key')) {
@@ -21,10 +29,26 @@ const insertLetter = {
         const box = Array.from(letterRow)[letterRowIndex].children[letterIndex];
         box.textContent = event.key;
         box.classList.add('filled-letter');
+        userAnswer.push(event.key.toUpperCase());
         letterIndex++;
       }
     }
   },
 };
 
+const checkWord = {
+  next: (event) => {
+    if (event.key === 'Enter') {
+      onUserWinOrLose$.next();
+    }
+  },
+};
+
 onKeyDown$.subscribe(insertLetter);
+onKeyDown$.subscribe(checkWord);
+onUserWinOrLose$.subscribe(() => {
+  const currentLetterRow = Array.from(letterRow)[0];
+  for (let i = 0; i < 5; i++) {
+    currentLetterRow.children[i].classList.add('letter-green');
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,15 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
   },
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: ['source-map-loader'],
+      },
+    ],
+  },
+  ignoreWarnings: [/Failed to parse source map/],
 };
